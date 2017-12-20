f3b07eb3a8c67a11da03950e3c873eb97dc24231
Travis Petersen <travis.e.petersen@gmail.com>
2017-12-20 23:01:14 +0000

    add calendar facts array

---
+++ lib/calendar-facts.rb
@@ -0,0 +1,39 @@
+facts = 
+[
+  'Did you know that ',
+[  
+  ['the ', ['Fall ', 'Spring '], 'Equinox '],
+  ['the ', ['Winter ', 'Summer '], ['Solstice ', 'Olympics ']],
+  ['the ', ['earliest ', 'latest '], ['sunrise ', 'sunset ']],
+  ['daylight ', ['saving ', 'savings '], 'time '],
+  ['leap ', ['day ', 'year ']],
+  'Easter ',
+  ['the ', ['harvest ', 'super ', 'blood '], 'moon '],
+  'Toyota Truck Month ',
+  'Shark Week '
+],   
+
+[
+  ['happens ', ['earlier ', 'later ', 'at the wrong time '], 'every year '],
+  ['drifts out of sync with the ', ['sun ', 'moon ', 'zodiac ', 'atomic clock in Colorado ', [['Gregorian ', 'Mayan ', 'Lunar ', 'iPhone '], 'calendar ']]],
+  ['might ', ['not happen ', 'happen twice '], 'this year ']
+],
+  'because of ',
+[
+  ['time zone legislation in ', ['Indiana', 'Arizona', 'Russia']],
+  'a decree by the Pope in the 1500\'s',
+  [['precession ', 'libration ', 'nutation ', 'libation ', 'eccentricity ', 'obliquity '], 'of the ', ['moon', 'sun', 'Earth\'s axis', 'Equator', 'Prime Meridian', [['International Date ', 'Mason-Dixon '], 'Line']]],
+  'magnetic field reversal',
+  ['an arbitrary decision by ', ['Benjamin Franklin', 'Isaac Newton', 'FDR']],
+],
+  '? Apparently ',
+[
+  'it causes a predictable increase in car accidents.',
+  'that\'s why we have leap seconds.',
+  'scientists are really worried.',
+  ['it was even more extreme during the ', ['Bronze Age.', 'Ice Age.', 'Cretaceous.', '1990\'s']],
+  ['there\'s a proposal to fix it, but it ', ['will never happen.', 'actually makes things worse.', 'is stalled in Congress.', 'might be unconstitutional.']],
+  'is getting worse and no one knows why.'
+]  
+  
+]

63b796439b566600c7eb22fdbd94d87ba32d1019
Travis Petersen <travis.e.petersen@gmail.com>
2017-12-20 23:05:43 +0000

    add menu

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -5,7 +5,11 @@ class CLI
   end
   
   def display_menu
-    
+    menu = <<-DOC
+      Welcome to XKCD Calendar Facts!
+      Hit <ENTER> to learn an interesting calendar fact!
+      <press any other key to exit>
+    DOC
   end
   
 end

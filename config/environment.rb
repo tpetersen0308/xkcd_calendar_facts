6654f7b929040931989f3b1a2ee097d99e2fb4b0
Travis Petersen <travis.e.petersen@gmail.com>
2017-12-20 23:15:47 +0000

    add environment file

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+require 'xkcd_calendar_facts'
+require "xkcd_calendar_facts/version"
+require_relative '../lib/calendar-facts.rb'
+require_relative '../lib/CLI.rb'
+require_relative '../fact-generator.rb'

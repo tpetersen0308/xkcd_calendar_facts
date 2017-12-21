a830c2f7ca1c4ee70ff6268465268de07356c784
Travis Petersen <travis.e.petersen@gmail.com>
2017-12-21 00:08:17 +0000

    add methods

--- lib/xkcd_calendar_facts/fact-generator.rb
+++ lib/xkcd_calendar_facts/fact-generator.rb
@@ -5,5 +5,21 @@ class FactGenerator
     @fact = String.new
   end
   
+  def random_path(arr)
+    arr.each do |el|
+      if el.class == String
+        @fact += el
+      elsif el.flatten.length != el.length
+        unknown = el.sample
+        random_path(unknown) if unknown.class == Array
+        @fact += unknown if unknown.class == String
+      else
+        @fact += el.sample
+      end
+    end
+  end
   
+  def print_random_path
+    puts @fact
+  end
 end
